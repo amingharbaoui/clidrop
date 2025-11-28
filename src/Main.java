f2eca65f1a88cefbb907da742c06d5fe654bae32
Amin <132141736+amingharbaoui@users.noreply.github.com>
2025-11-28 16:59:44 +0100

    fixed spacing, text-size in responsive layout + added Jetty server to serve webpage on localhost with port 3000

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 
+public static void main(String[] args) throws Exception {
+
+    Server server = new Server(3000);
+    ServletContextHandler handler = new ServletContextHandler();
+    ServletHolder holder = new ServletHolder(new DefaultServlet());
+
+    holder.setInitParameter("resourceBase", "static");
+    holder.setInitParameter("dirAllowed", "false");
+
+    handler.addServlet(holder, "/*");
+    server.setHandler(handler);
+
+    server.start();
+    server.join();
 }
+
+
